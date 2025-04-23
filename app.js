a394c1dfd2244851d1ba14cfc3c1a7f6a067263e
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-04-23 09:07:45 +0200

    homepage

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ const port = 8000
 
 app.set('view engine', 'pug')
 
+app.use(session({
+    secret: 'chathygge',
+    resave: true,
+    saveUninitialized: true
+}))
 
+app.get('/login', (req, res) => {
+    const { username, password } = req.session.body
+    res.render('login')
+})
+
+app.get('/', (req, res) => {
+    res.render('homepage')
+})
 
 app.listen(port, () => {
     console.log("Listening on port 8000");

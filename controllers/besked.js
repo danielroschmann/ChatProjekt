8c750fdc85a1528fd5edda4c81fd2ad5ea7d34f1
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-04-28 11:33:40 +0200

    opret besked funktion

--- controllers/besked.js
+++ controllers/besked.js
@@ -1,9 +1,10 @@
-import Besked from "../models/Besked"
-import { BESKED_FIL, læsJSON, CHAT_FIL, gemJSON, EJER_FIL } from "./filData"
-import { læsJSON } from "./filData"
+import Besked from "../models/Besked.js"
+import { BESKED_FIL, læsJSON, CHAT_FIL, gemJSON, EJER_FIL } from "./filData.js"
+import session from 'express-session'
 
 export const getAllMessagesInChat = (req, res) => {
     const chatId = Number(req.params.id)
+    let chats = læsJSON(CHAT_FIL)
     const chat = chats.find(c => c.id === chatId)
     console.log(chat)
     
@@ -12,6 +13,7 @@ export const getAllMessagesInChat = (req, res) => {
 
 export const getSingleMessage = (req, res) => {
     const beskedId = Number(req.params.id)
+    let chats = læsJSON(CHAT_FIL)
     const chat = chats.find(c => c.id === 1) 
     const besked = chat.beskeder.find(b => b.id === beskedId)
     console.log(besked)
@@ -23,27 +25,32 @@ export const createMessage = (req, res) => {
     let besked = req.body.besked;
     let ejer = læsJSON(EJER_FIL).find(u => u.navn === req.session.username);
     let chatId = req.body.chatId;
+    console.log("Hentet chatId: " + chatId)
     
     let chatArr = læsJSON(CHAT_FIL);
+    console.log(chatArr)
     let beskedArr = læsJSON(BESKED_FIL);
+
+    console.log(beskedArr.length)
     
     let id = beskedArr.length > 0 ? beskedArr[beskedArr.length - 1].id + 1 : 1; // korrekt ID
     let dato = new Date().toLocaleString(); // korrekt dato
     
     let nyBesked = new Besked(id, besked, dato, ejer, chatId);
     
-    let chat = chatArr.find(c => c.id === chatId);
+    let chat = chatArr.find(c => c.id == chatId);
+    console.log(chat)
     if (!chat) {
         return res.status(404).json({ error: "Chat ikke fundet" });
     }
     
     chat.beskeder.push(nyBesked);
     beskedArr.push(nyBesked); // husk også at gemme den i beskedfilen
-    
+
     gemJSON(CHAT_FIL, chatArr);
     gemJSON(BESKED_FIL, beskedArr);
     
-    res.status(201).json(nyBesked); // svarer med den nye besked
+    res.redirect(`/chats/${chatId}/messages`)
 };
 
 

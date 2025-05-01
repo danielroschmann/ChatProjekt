420fa10c187c4a63b981d9eca2a951e10e355b57
danielroschmann <eaa24daro@students.eaaa.dk>
2025-05-01 11:08:57 +0200

    .

--- controllers/bruger.js
+++ controllers/bruger.js
@@ -3,8 +3,7 @@ import { EJER_FIL, BESKED_FIL } from "./filData.js"
 import Ejer from "../models/Ejer.js"
 
 export const createUser = async (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
+    const {username, password} = req.body
     const dato = new Date().toLocaleDateString()
     let brugere = læsJSON(EJER_FIL)
     if (brugere === undefined) {

4ec69a6545798d6dfc251a1f036d65bc4ca628a8
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-05-11 22:04:29 +0200

    .

--- utils/helperUtils.js
+++ utils/helperUtils.js
@@ -1,10 +1,11 @@
 
 export const generateUniqueId = () => {
-    const length = 21
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    const result = new Array(length)
-    for (let i = 0; i < length; i++) {
-        result[i] = characters.charAt(Math.floor(Math.random() * characters.length))
-    }
-    return result.join('')
+    return crypto.randomUUID()
+    // const length = 21
+    // const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    // const result = new Array(length)
+    // for (let i = 0; i < length; i++) {
+    //     result[i] = characters.charAt(Math.floor(Math.random() * characters.length))
+    // }
+    // return result.join('')
 }
